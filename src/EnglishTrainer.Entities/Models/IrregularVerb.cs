63de9d6b058d800330bbfaf0e735ba1fde6dacb4
DzmitryHalubovich <buxarik493@gmail.com>
2023-04-22 22:28:33 +0300

    Add dbContext

---
+++ src/EnglishTrainer.Entities/Models/IrregularVerb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTrainer.Entities.Models
+{
+    public class IrregularVerb
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("infinitive")]
+        [MaxLength(50)]
+        public string Infinitive { get; set; }
+
+        [Column("past_simple")]
+        [MaxLength(50)]
+        public string PastSimple { get; set; }
+
+        [Column("past_participle")]
+        [MaxLength(50)]
+        public string PastParticiple { get; set; }
+
+        [Column("short_translate")]
+        [MaxLength(50)]
+        public string ShortTranslate { get; set; }
+    }
+}
---
+++ src/EnglishTrainer.Entities/Models/Word.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTrainer.Entities.Models
+{
+    public class Word
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("name")]
+        [Required(ErrorMessage = "The word name is a required field.")]
+        [MaxLength(50, ErrorMessage = "Maximum lenth for the Name is 50 characters.")]
+        public string Name { get; set; }
+
+        [Column("translations")]
+        [MaxLength(256, ErrorMessage = "Maximum lenth for the Translations is 256 characters.")]
+        public string Translations { get; set; }
+
+        [Column("description")]
+        [MaxLength(512, ErrorMessage = "Maximum lenth for the Description is 512 characters.")]
+        public string? Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+
+
+        public ICollection<Example> Examples { get; set; }
+    }
+}
