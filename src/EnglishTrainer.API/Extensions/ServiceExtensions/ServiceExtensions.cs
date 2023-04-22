ba64ac938afd1db08b7acead2bab7a8f764d2747
DzmitryHalubovich <buxarik493@gmail.com>
2023-04-22 18:54:25 +0300

    add app configuration in ServiceExtensions file

---
+++ src/EnglishTrainer.API/Extensions/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace EnglishTrainer.API.Extensions.ServiceExtensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => 
+            services.AddCors(
+                options => 
+                { 
+                    options.AddPolicy("CorsPolicy", builder => builder
+                        .AllowAnyOrigin() //WithOrigins("https://example.com")
+                        .AllowAnyMethod() //WithMethods("POST", "GET")
+                        .AllowAnyHeader()); //WithHeaders("accept", "content-type") method to allow only specific headers
+                });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) => 
+            services.Configure<IISOptions>(options => {
+        });
+    }
+}
--- src/EnglishTrainer.API/Program.cs
+++ src/EnglishTrainer.API/Program.cs
@@ -1,3 +1,6 @@
+using EnglishTrainer.API.Extensions.ServiceExtensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,16 +10,41 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region From ServiceExtensions
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else 
+{ 
+    app.UseHsts(); 
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions 
+{ 
+    ForwardedHeaders = ForwardedHeaders.All 
+});
+
+app.UseRouting();
 
 app.UseAuthorization();
 
